f581b67d373403ef707878a89f80fe3794da5d1a
Jakob <31919330+Jakobzs@users.noreply.github.com>
2021-08-23 01:24:24 +0200

    Open debug console to show output

--- src/cheat.rs
+++ src/cheat.rs
@@ -1,9 +1,15 @@
 use std::ptr;
 
+use windows_bindings::Windows::Win32::System::Console::AllocConsole;
+
 pub fn start() {
-    println!("TESTY");
-    /*let p = 0x0900 as *const u32;
+    // Debug console
+    unsafe { AllocConsole() };
+
+    println!("Hello from DLL!");
+
+    let p = 0x282A6325270 as *const u32;
     let n = unsafe { ptr::read(p) };
 
-    println!("{}", n);*/
+    println!("{}", n);
 }
--- windows_bindings/build.rs
+++ windows_bindings/build.rs
@@ -1,6 +1,7 @@
 fn main() {
     windows::build!(
         Windows::Win32::System::SystemServices::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH, DLL_THREAD_ATTACH, DLL_THREAD_DETACH},
-        Windows::Win32::Foundation::{BOOL, HINSTANCE}
+        Windows::Win32::Foundation::{BOOL, HINSTANCE},
+        Windows::Win32::System::Console::AllocConsole,
     );
 }
