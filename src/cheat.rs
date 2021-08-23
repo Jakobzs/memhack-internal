84f2643a38d401a1fb80ef55ec2c54cce6209471
Jakob <31919330+Jakobzs@users.noreply.github.com>
2021-08-23 09:41:02 +0200

    Opening and closing debug console with Result

--- src/cheat.rs
+++ src/cheat.rs
@@ -1,13 +1,17 @@
-use std::os::windows::process;
+use std::error::Error;
 
 use windows_bindings::Windows::Win32::{
-    Foundation::PSTR,
-    System::{Console::AllocConsole, LibraryLoader::GetModuleHandleA},
+    Foundation::{BOOL, PSTR},
+    System::{
+        Console::{AllocConsole, FreeConsole},
+        Diagnostics::Debug::GetLastError,
+        LibraryLoader::GetModuleHandleA,
+    },
 };
 
 pub fn start() {
     // Debug console
-    unsafe { AllocConsole() };
+    open_debug_console().unwrap();
 
     println!("Hello from DLL!");
 
@@ -16,7 +20,27 @@ pub fn start() {
     println!("Test: {:#01x}", process_base_address.0);
 
     let p = process_base_address.0 as *const u32;
-    let n = unsafe { std::ptr::read(p) };
+    let n = unsafe { *p };
 
     println!("{}", n);
+}
+
+fn open_debug_console() -> Result<(), Box<dyn Error>> {
+    if unsafe { AllocConsole() }.0 == 0 {
+        let testy = unsafe { GetLastError() };
+
+        let test = testy.0;
+
+        Err("failed opening console, GetLastError")?
+    } else {
+        Ok(())
+    }
+}
+
+fn close_debug_console() -> Result<(), Box<dyn Error>> {
+    if unsafe { FreeConsole() }.0 == 0 {
+        Err("failed closing console")?
+    } else {
+        Ok(())
+    }
 }
--- windows_bindings/build.rs
+++ windows_bindings/build.rs
@@ -2,7 +2,8 @@ fn main() {
     windows::build!(
         Windows::Win32::System::SystemServices::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH, DLL_THREAD_ATTACH, DLL_THREAD_DETACH},
         Windows::Win32::Foundation::{BOOL, HINSTANCE, PSTR},
-        Windows::Win32::System::Console::AllocConsole,
+        Windows::Win32::System::Console::{AllocConsole, FreeConsole},
         Windows::Win32::System::LibraryLoader::GetModuleHandleA,
+        Windows::Win32::System::Diagnostics::Debug::GetLastError,
     );
 }
